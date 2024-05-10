7456008ba9eba52bf4c1992693052b45180773e7
KarenMarcela <kmbayonam@ufpso.edu.co>
2024-05-10 15:01:11 -0500

    load

--- js/functions.js
+++ js/functions.js
@@ -44,6 +44,9 @@ function load_page(){
         showConfirmButton: false,
         timer:2000
       });
+
+      document.getElementById("text").innerText = "El array a calcular es: "+array_num; 
+      document.getElementById("impresion").value = array_num; 
 }
 function send_form(){
     let name      = document.getElementById("name").value;
@@ -70,6 +73,104 @@ function send_form(){
     }
     else{
         document.getElementById("print").innerText = "Su nombre es: "+name+" "+last_name;
+        document.getElementById("name").style.border = "2px solid green";
+        document.getElementById("last_name").style.border = "2px solid green";
     }
+}
+
+//ARRAYS
+var array_semana = ["Lunes", "Martes", "Mièrcoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+var array_gra1= [1,2,2.5,"abc","Soy Karen"]
+var array_num= [1,2,3,4,5,6,7,8,9];
+
+var array_multi = [
+  {name: "Karen" , age: 17, color: "Rojo"},
+  {name: "Karol" , age: 18, color: "Blanco"},
+  {name: "Daniela" , age: 20, color: "Negro"},
+  {name: "Andrey" , age: 24, color: "Verde"}
+];
+
+var json_ejem = {
+  name: "Karen",
+  lasname: "Bayona",
+  phone :"3143926679" ,
+  email: "kmbayonam@ufpso.edu.co"
+};
+
+console.log(array_semana);
+console.log(array_gra1);
+console.log(array_num);
+console.log(array_multi);
+console.log(json_ejem);
+
+function action(){
+  // alert(array_num);
+  // Swal.fire(""+array_num); //deja un expacio y le concateno el array_num porque el swal.fire solo muestra valores tipo STRING
+  // console.log(array_num);
+  // console.log(array_num.length);
+  var result=0;
+  for(let i=0;i<array_num.length;i++){
+    result = result+array_num[i];
+  }
+ 
+  document.getElementById("result").innerHTML = "<strong>El resultado es: "+result+" </strong>";
+  document.getElementById("result").style.color = "green";
+
+  var total=0;
+  for(var j=0;j<array_multi.length;j++){
+    total = array_multi[j].age + total;
+  }
+  alert(total);
+
+  var total_dos = 0;
+  var w= 0;
+  //CON UN WHILE
+  while(w<array_multi.length){
+    total_dos = array_multi[w].age + total_dos;
+    w++;
+  }
+  alert(total_dos);
+}
+
+var array_numerico = [1,2,3,4,5,6,7,8,9];
+function eliminar(){
+  array_delete = array_numerico.pop(num);
+  console.log(array_delete);
+  console.log(array_numerico);
+  document.getElementById("impresion" ).value = array_numerico;
+}
+
+function agregar(){
+  let num = document.getElementById("num").value;
+  let array_add = array_numerico.push(num);
+  array_add = array_num.push(10);
+  console.log(array_add);
+  console.log(array_numerico);
+  document.getElementById("impresion" ).value = array_numerico;
+
+}
+
+function agregar_prim(){
+  let num = document.getElementById("num").value;
+  let array_add = array_numerico.unshift(num);
+  document.getElementById("impresion").value = array_numerico;
+}
+
+function eliminar_prim(){
+  let num = document.getElementById("num").value;
+  let array_add = array_numerico.shift(num);
+  document.getElementById("impresion").value = array_numerico;
+}
+
+function reves(){
+  document.getElementById("impresion").value = array_numerico.reverse();
+}
+
+function limpiar(){
+  document.getElementById("num").value = "";
+  document.getElementById("numDelete").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("last_name").value = "";
+  document.getElementById("result").innerText= "";
 }
 
